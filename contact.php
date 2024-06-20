d96558f3af0a343d03c0ce87b9468e05e191f809
Favour <ofavourite001@gmail.com>
2024-06-20 15:06:13 +0100

    form4

--- contact.php
+++ contact.php
@@ -39,21 +39,4 @@
     } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
     }
-        // $gname = $_POST['gname'];
-        // $gemail = $_POST['gemail'];
-        // $cname = $_POST['cname'];
-        // $cage = $_POST['cage'];
-        // $tmessage = $_POST['tmessage'];
-        
-        // $to = "greatuccschools@gmail.com";
-        // $subject = "Website Contact Form: $gname on behalf of $cname";
-        // $message = "Guardian Name: $gname\nGuardian Email: $gemail\nChild Name: $cname\nChild Age: $cage\nMessage: $tmessage";
-        // $headers = "UCCSchools <admin@uccschools.com> \r\n";
-        // $headers .= "Reply-To: $gemail \r\n";
-        // $headers .= "Content-Type: text/plain; charset=UTF-8 \r\n";
-        // if (mail($to, $subject, $message, $headers)) {
-        //     echo "Thank you for contacting us! We will get back to you soon.";
-        //     } else {
-        //     echo "There was an error sending your message. Please try again later.";
-        //     }
-    ?>
+
