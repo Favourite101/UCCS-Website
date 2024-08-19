1afb397dc328e28c07f5ea5146fb9baf39525b70
Favourr <ofavourite001@gmail.com>
2024-08-19 13:18:45 +0100

    final fix hopefully

--- UCCSchools/wsgi.py
+++ UCCSchools/wsgi.py
@@ -4,13 +4,16 @@ WSGI config for UCCSchools project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UCCSchools.settings')
+# Check for the WEBSITE_HOSTNAME environment variable to see if we are running in Azure Ap Service
+# If so, then load the settings from production.py
+settings_module = 'UCCSchools.production' if 'WEBSITE_HOSTNAME' in os.environ else 'UCCSchools.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
