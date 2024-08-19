f775c4fdd6d011ae6c938d27a0fcc6a73cb894a8
Favourr <ofavourite001@gmail.com>
2024-08-19 13:05:14 +0100

    edit settings

--- UCCSchools/settings.py
+++ UCCSchools/settings.py
@@ -126,13 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'UCCApp/static',
-]
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
